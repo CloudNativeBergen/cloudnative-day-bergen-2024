e521648acb9eeb98e24c1df4e7e162bf2215e352
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-04-05 18:29:31 +0200

    Update logo and tagline

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ export function Hero() {
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
           <h1 className="font-display text-5xl font-bold tracking-tighter text-blue-600 sm:text-7xl">
-            <span className="sr-only">CloudNative Day Bergen - </span>The all things cloud & Kubernetes event.
+            <span className="sr-only">CloudNative Day Bergen - </span>Full day Cloud and Kubernetes in Bergen.
           </h1>
           <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-blue-900">
             <p>
