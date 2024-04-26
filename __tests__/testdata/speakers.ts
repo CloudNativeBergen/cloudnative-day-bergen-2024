75275f515668a1feb6421a9e595b56492d85c95f
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-04-26 11:13:48 +0200

    Make tests work

---
+++ __tests__/testdata/speakers.ts
@@ -0,0 +1,24 @@
+import { Speaker } from "@/types/proposal";
+
+const spekaers = [
+  {
+    _id: '1',
+    name: 'John Doe',
+    title: 'Acme Inc.',
+    email: 'john@acme.com',
+    is_diverse: false,
+    is_first_time: true,
+    is_local: true
+  },
+  {
+    _id: '2',
+    name: 'Jane Doe',
+    title: 'Acme Inc.',
+    email: 'jane@acme.com',
+    is_diverse: true,
+    is_first_time: false,
+    is_local: false
+  }
+] as Speaker[];
+
+export default spekaers;
--- src/app/api/cfp/route.test.ts
+++ src/app/api/cfp/route.test.ts
@@ -1,21 +1,21 @@
 /**
  * @jest-environment node
  */
-import { expect, it, jest } from '@jest/globals';
+import { expect, it, jest, beforeEach } from '@jest/globals';
 import { GET } from './route';
+import { clientWrite } from '@/lib/sanity/client';
+import proposals from '../../../../__tests__/testdata/proposals';
 
-jest.mock('@/lib/proposal/sanity', () => ({
-  getProposals: jest.fn(),
-}));
-
-jest.mock('@/lib/sanity/client', () => ({
-  clientRead: jest.fn(),
-  clientWrite: jest.fn(),
-}));
+beforeEach(() => {
+  clientWrite.fetch = jest.fn<() => Promise<any>>().mockResolvedValue(
+    proposals.filter((p) => p.speaker?._id === '1')
+  );
+});
 
 it('should return data with status 200', async () => {
   const response = await GET();
   const body = await response.json();
   expect(response.status).toBe(200);
-  expect(body.message).toBe('Hello, world!');
+  expect(body.proposals).toHaveLength(3);
+  expect(body.proposals[0].speaker?._id).toBe('1');
 });
