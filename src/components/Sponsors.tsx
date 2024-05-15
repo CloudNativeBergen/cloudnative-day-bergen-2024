98d98ee548bbcd4a8a3163c1b9f56b689b154a9e
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-05-15 06:56:01 +0200

    Update sponsor layout

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -22,9 +22,9 @@ export function Sponsors() {
     <section id="sponsors" aria-label="Sponsors" className="py-20 sm:py-32">
       <Container>
         <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-blue-900 sm:text-5xl">
-          Current sponsorships for our workshops and speakers.
+          Our awesome sponsors that makes this event happening!
         </h2>
-        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
+        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-4 md:gap-x-16 lg:gap-x-32">
           {sponsors.map((sponsor, i) => (
             <div
               key={`${sponsor.name}-${i}`}
