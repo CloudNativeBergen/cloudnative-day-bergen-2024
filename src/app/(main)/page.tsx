46f448b6f777a4ef96181789f0610d43e4a56b1b
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-10-11 20:01:13 +0200

    Only cache schedule for 1 hour

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,9 @@
 import { Hero } from '@/components/Hero'
 import { Newsletter } from '@/components/Newsletter'
-import { FeaturedSpeakers } from '@/components/FeaturedSpeakers'
 import { Schedule } from '@/components/Schedule'
 import { Speakers } from '@/components/Speakers'
 import { Sponsors } from '@/components/Sponsors'
-import { clientReadCached as clientRead } from '@/lib/sanity/client'
+import { clientReadUncached as clientRead } from '@/lib/sanity/client'
 import { Schedule as ScheduleType, scheduleToTracks } from '@/lib/schedule'
 
 export const revalidate = 3600
@@ -12,7 +11,11 @@ export const revalidate = 3600
 async function getData() {
   return await clientRead.fetch<ScheduleType[]>(
     `*[_type == "schedule"]{date, time_start, time_end, track->{number, title, description}, talk->{title, speaker->{name, "slug": slug.current, title, "image": image.asset->url}}} | order(track.number asc, time_start asc)`,
-  )
+    {}, {
+    next: {
+      revalidate: revalidate,
+    }
+  })
 }
 
 export default async function Home() {
