622f013389d2f181bce4e64ef16a6badf969ebe1
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-11-23 22:40:11 +0100

    Fix invalid <Link> with <a> child

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,9 +20,7 @@ export function Header() {
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
         <div className="mt-10 lg:mt-0 lg:grow lg:basis-0">
           <Link href="/">
-            <a>
-              <Logo className="h-12 w-auto text-slate-900" />
-            </a>
+            <Logo className="h-12 w-auto text-slate-900" />
           </Link>
         </div>
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
