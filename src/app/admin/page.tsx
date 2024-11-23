4a410ad28af90e92c328d9677269383dbfd3645e
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-11-23 21:29:56 +0100

    Experimental new admin layout

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import AdminLayout from "@/components/AdminLayout";
+
+export default function Admin() {
+  return (
+    <AdminLayout>
+      <h1>
+        Admin Page
+      </h1>
+    </AdminLayout>
+  )
+}
